48011cc211e92032fb34cffd1e45bbe9ab2fbddd
c9long <c9long@uwaterloo.ca>
2024-12-12 12:34:22 -0500

    finished user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'home#index'
+  root 'friends#index'
   get 'home/about'
   get "up" => "rails/health#show", as: :rails_health_check
 end
